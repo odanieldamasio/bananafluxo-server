7c6478eba6fe8199e86cb7f2557eaafbc5259e7f
Daniel Damasio <damasio.dan7@gmail.com>
2025-08-28 21:34:50 -0300

    feat: 🍌 swagger docs implemented

--- src/tenants/dto/create-tenant.dto.ts
+++ src/tenants/dto/create-tenant.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTenantDto {
+  @ApiProperty({ example: 'Banano MEI' })
   name: string;
+
+  @ApiProperty({ example: 'banano-mei' })
   slug: string;
 }
