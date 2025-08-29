6e249c2ce7a0c0d13e6977f24cedccda5b6639d2
Daniel Damasio <damasio.dan7@gmail.com>
2025-08-28 22:55:31 -0300

    feat: 🍌 class validation library implemented

---
+++ src/prisma/prisma-errors.enum.ts
@@ -0,0 +1,5 @@
+export enum PrismaErrors {
+  UniqueConstraint = 'P2002',
+  RecordNotFound = 'P2025',
+  ForeignKeyConstraint = 'P2003',
+}
--- src/tenants/dto/create-tenant.dto.ts
+++ src/tenants/dto/create-tenant.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateTenantDto {
   @ApiProperty({ example: 'Banano MEI' })
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ example: 'banano-mei' })
+  @IsNotEmpty()
   slug: string;
 }
