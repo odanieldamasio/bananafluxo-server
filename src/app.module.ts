eba32a9d2c247ae4fdfa6ad739984f4e7260b646
Daniel Damasio <damasio.dan7@gmail.com>
2025-08-30 00:24:36 -0300

    feat: 🍌 implements auth passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,18 +2,21 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/response-user.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find user by id' })
   async findOne(@Param('id') id: string): Promise<UserResponseDto | null> {
-    return this.usersService.findOne(id);
+    return this.usersService.findOneById(id);
   }
 }
